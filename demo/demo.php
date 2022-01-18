54e788d8542475c357ddce019662d3fed1a7c887
mojun <mojun@longmaster.com.cn>
2022-01-18 14:47:06 +0800

    修改

--- demo/demo.php
+++ demo/demo.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "./vendor/autoload.php";
+require_once "../vendor/autoload.php";
 
 $request = \Dreammo\AntOpenAllianceChain\Request::getInstance('accessId', 'accessKeyContent');
 // 开启握手获取通信token
